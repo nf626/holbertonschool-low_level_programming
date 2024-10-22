b77ca3a2b17914236a0d0860296d4d6a80394c92
Nigel Feng <10083@holbertonstudents.com>
2024-10-22 01:24:24 +0000

    4. Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/**
+/*
+ * 4.Hello,puts 
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
+    puts("Programming is like building a multilingual puzzle\n");
 
-  return (0);
+    return (0);
 }
