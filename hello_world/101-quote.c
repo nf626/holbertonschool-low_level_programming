db11d09deaba5ded815b7c70c6bf5f2e0def1e91
Nigel Feng <10083@holbertonstudents.com>
2024-10-23 00:48:09 +0000

    9. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
-echo -e "and that piece of art is useful\" - Dora Korpar, 2015-10-19" > q 2> q
+
+/**
+ * main - Writes and prints Dora Korpar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+return (0);
+}
