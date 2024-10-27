832d3c8c538f8f13e688911c940f04172670e964
Nigel Feng <10083@holbertonstudents.com>
2024-10-27 12:51:50 +0000

    11. 98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98.
+ * @n: Formal parameter to pass.
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+if (n == 98)
+{
+_putchar(n);
+_putchar('\n');
+}
+else
+{
+_putchar(n);
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+_putchar(n);
+_putchar('\n');
+}
+else
+{
+_putchar(n);
+_putchar(',');
+_putchar(' ');
+}
+
+}
+}
