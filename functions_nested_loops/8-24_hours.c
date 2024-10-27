6b49eef3809e7f4a0200acb1d8ff2a986b19af0f
Nigel Feng <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>
2024-10-27 07:15:51 +0000

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,8 +9,19 @@
  */
 void jack_bauer(void)
 {
-  for ()
-    {
-    }
-  return (0);
+int hours, minutes;
+
+for (hours = 0; hours <= 23; hours++)
+{
+for (minutes = 0; minutes <= 59; minutes++)
+{
+_putchar(hours / 10 + '0');
+_putchar(hours % 10 + '0');
+_putchar(':');
+_putchar(minutes / 10 + '0');
+_putchar(minutes % 10 + '0');
+_putchar('\n');
+}
+}
+
 }
