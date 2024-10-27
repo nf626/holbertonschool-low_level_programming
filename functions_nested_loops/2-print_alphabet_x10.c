d3c1e536ce8e5942e799239dd7d9297acac3543d
Nigel Feng <10083@holbertonstudents.com>
2024-10-27 01:59:45 +0000

    2. 10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabetx10 - Prints 10 x alphabet in lowercase.
+ *
+ */
+void print_alphabet_x10(void)
+{
+char i;
+int j;
+
+for (i = 1; i <= 10; i++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+
+}
