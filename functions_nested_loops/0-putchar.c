751c48bd16b474defc4ad3fcfaefc8d39bfb6945
Nigel Feng <10083@holbertonstudents.com>
2024-10-27 01:15:50 +0000

    0. _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Prints _putchar followed by newline.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-  _putchar('A');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-  return 0;
+return (0);
 
 }
