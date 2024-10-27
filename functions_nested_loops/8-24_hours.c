d568c126fce9f27fbc76f4bb5e1345ad3de7a5db
Nigel Feng <10083@holbertonstudents.com>
2024-10-27 04:08:59 +0000

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+  for ()
+    {
+    }
+  return (0);
+}
