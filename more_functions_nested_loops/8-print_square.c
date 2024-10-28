7b8f0a6ac2413eb0f82a555bb0cb1836d8541427
Nigel Feng <10083@holbertonstudents.com>
2024-10-28 07:06:31 +0000

    8. You are so much sunshine in every square inch

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,15 +1,31 @@
 #include "main.h"
 
 /**
- *
- *
+ * print_square - prints a square.
+ * @size: size of square.
  *
  */
 
 void print_square(int size)
 {
-  
-  while ()
-    {
-    }
+int row, coloumn;
+coloumn = 0;
+
+if (size < 1)
+{
+_putchar('\n');
+
+while (coloumn < size)
+{
+row = 0;
+
+while (row < size)
+{
+_putchar('#');
+row++;
+}
+_putchar('\n');
+coloumn++;
+}
+}
 }
