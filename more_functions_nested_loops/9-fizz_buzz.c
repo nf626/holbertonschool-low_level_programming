e782a2f1cd32a9484456377c8de04cb4a59b1649
Nigel Feng <10083@holbertonstudents.com>
2024-10-28 08:21:00 +0000

    9. Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,10 +12,23 @@
  */
 int main (void)
 {
-  int x;
+  int i;
 
-  x = 1 % 3;
+  for (i = 0; i <= 100; i++)
+    {
+      if ((i % 3) == 0)
+	{
+	  printf("Fizz ");
+	}
+      else if ((i % 5) == 0)
+	{
+	  printf("Buzz ");
+	}
+      else if (((i % 3) == 0) && ((i % 5) == 0))
+	{
+	  printf("FizzBuzz ");
+	}
+    }
   
-  
-  return (x);
+  return (0);
 }
