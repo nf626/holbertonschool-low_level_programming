260858217441afc17d8b403a908d80b80029ccce
Nigel Feng <10083@holbertonstudents.com>
2024-10-28 04:33:50 +0000

    7. I feel like I am diagonally parked in a parallel universe

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,4 +10,21 @@
 void print_diagonal(int n)
 {
 
+  int i, j;
+
+      for (i = 0; i < n; i++)
+	{
+	  for (j = 0; j < i; j++)
+	    {
+	      if (j == n)
+		{
+		  _putchar('\\');
+		}
+	      else
+		{
+		  _putchar(' ');
+		}
+	    }
+	  _putchar('\n');
+	}
 }
