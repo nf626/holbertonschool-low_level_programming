7fa941ad9682bc2d9356f75bc7402866aa9a2bb2
Nigel Feng <10083@holbertonstudents.com>
2024-11-01 14:30:33 +0000

    0. strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat -
+ *
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+  int i;
+
+  while (src[i] != '\0')
+    {
+      dest[i] + src[i];
+      i++
+	}
+
+  dest[i++] = '\0';
+
+  return (0);
+}
