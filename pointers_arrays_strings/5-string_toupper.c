4a1f42c7cd6f930c8b4dc56dc3a8e08047f38d93
Nigel Feng <10083@holbertonstudents.com>
2024-11-03 05:23:04 +0000

    5. Always look up

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,15 +8,17 @@
  */
 char *string_toupper(char *c)
 {
-  int i = 0, temp;
+  int i = 0;
+  
 
   while (c[i] != '\0')
     {
-      if (c[i] >= 'a' && c[i] <= 'z')
-	{
-	 temp = x
-	}
       i++;
     }
-  return ();
+  while (c[i] <= 'a' && c[i] >= 'z')
+    {
+     i++;
+    }
+  
+  return (c);
 }
