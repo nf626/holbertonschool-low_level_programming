ead23d94f0f5c9da942389860879344232ac5941
Nigel Feng <10083@holbertonstudents.com>
2024-11-03 04:35:28 +0000

    5. Always look up

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase.
+ * 
+ *
+ *
+ */
+char *string_toupper(char *c)
+{
+  int i = 0, temp;
+
+  while (c[i] != '\0')
+    {
+      if (c[i] >= 'a' && c[i] <= 'z')
+	{
+	 temp = x
+	}
+      i++;
+    }
+  return ();
+}
