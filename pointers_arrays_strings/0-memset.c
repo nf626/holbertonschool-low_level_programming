6c9a1e065b61578f55f7886c3d3673829ffa7a54
Nigel Feng <10083@holbertonstudents.com>
2024-11-04 12:17:25 +0000

    0. memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -3,15 +3,20 @@
 /**
  * _memset - fills memory with a constant byte.
  * @s: Starting address memory to be filled.
- * @b: Constant Byte.
+ * @b: Constant Byte to fill at number.
  * @n: Number of bytes to fill.
  *
  * Return: Pointer to s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int n = 95;
+unsigned int i = 0;
 
-  
-  return (0);
+while (i < n)
+{
+s[i] = b;
+i++;
+}
+
+return (s);
 }
