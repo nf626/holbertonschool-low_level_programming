2a9483991abf05154577740c5153b41c7fabc4f0
Nigel Feng <10083@holbertonstudents.com>
2024-11-05 23:07:43 +0000

    6. Chess is mental torture

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: Input coloumn.
+ *
+ * Return: 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+// char = 1 byte * row + col to find element in array[][]
+
+  char *pA = a;
+
+  while 
+}
