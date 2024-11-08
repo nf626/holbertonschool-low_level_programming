c5e94e618098c823b72f30fe7c09a48091783fca
Nigel Feng <10083@holbertonstudents.com>
2024-11-08 07:14:03 +0000

    1. Why is it so important to dream? Because, in my dreams we are together

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,7 +9,10 @@ void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
 {
-_print_rev_recursion(s + 1);
-_putchar(*s);
+return;
 }
+s = s + 1;
+_print_rev_recursion(s);
+s = s - 1;
+_putchar(*s);
 }
