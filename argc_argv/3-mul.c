812eca40b7c4fa79f2edd94d0a54fec0003030e7
Nigel Feng <10083@holbertonstudents.com>
2024-11-09 14:19:43 +0000

    3. Neither irony nor sarcasm is argument

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 {
 int sum = 0;
 
-if (argc > 1)
+if (argc != 3)
 {
 sum = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", sum);
 }
 else
 {
-printf("Error\n");
+printf("%s\n", "Error");
 }
 return (0);
 }
