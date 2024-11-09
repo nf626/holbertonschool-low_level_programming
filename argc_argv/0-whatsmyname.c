5ccc7b3ffb3afe18ff3a4b2c7fc436f062507e38
Nigel Feng <10083@holbertonstudents.com>
2024-11-09 12:47:42 +0000

    0. It ain't what they call you, it's what you answer to

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: contains the number of arguments passed to the program.
+ * @argv: one-dimensional array of strings.
+ *        Each string is one of the arguments
+ *        that was passed to the program.
+ *
+ * Return: string.
+ */
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
