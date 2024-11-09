ca987eb1789b3a2f11d98e250bf881df769310e9
Nigel Feng <10083@holbertonstudents.com>
2024-11-09 14:04:34 +0000

    3. Neither irony nor sarcasm is argument

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: contains the number of arguments passed to the program.
+ * @argv: one-dimensional array of strings.
+ *        Each string is one of the arguments
+ *        that was passed to the program.
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+int sum = 0;
+
+if (argc > 1)
+{
+sum = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", sum);
+}
+else
+{
+printf("Error\n");
+}
+return (0);
+}
