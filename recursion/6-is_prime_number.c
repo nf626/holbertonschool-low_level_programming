aaeab79703dd3c07b21d754574b3c31d703659ed
Nigel Feng <10083@holbertonstudents.com>
2024-11-09 05:58:01 +0000

    6. Inception. Is it possible?

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,22 +2,26 @@
 
 /**
  * checker - find if prime number.
- * @x: Value passed by is_prime_number..
+ * @x: Value passed by is_prime_number.
+ * @y: 2.
  *
  * Return: value back to is_prime_number.
  */
 int checker(int x, int y)
 {
-  if (x == 1)
-    {
-      return (1);
-    }
-  else if(x > 2)
-    {
-      return (0);
-    }
- 
-  return (checker(x, y + 1);
+if (x == y)
+{
+return (1);
+}
+else if (x % y == 0)
+{
+return (0);
+}
+else if (y >= (x / 2))
+{
+return (1);
+}
+return (checker(x, y + 1));
 }
 
 /**
@@ -28,10 +32,10 @@ int checker(int x, int y)
  */
 int is_prime_number(int n)
 {
-  if (n < 0)
-    {
-      return (0);
-    }
+if (n <= 1)
+{
+return (0);
+}
 
-  return (checker(int n, 0));
+return (checker(n, 2));
 }
