61b1fbd5d75e0961491b22462db19684f34ca341
Nigel Feng <10083@holbertonstudents.com>
2024-11-09 04:43:16 +0000

    6. Inception. Is it possible?

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * is_prime_number - input integer is a prime number.
+ * @n: Value.
+ *
+ * Return: 1 is prime number, 0 not prime number.
+ */
+int primeNum(int x, int y)
+{
+}
+
+/**
+ * is_prime_number - input integer is a prime number.
+ * @n: Value.
+ *
+ * Return: 1 is prime number, 0 not prime number.
+ */
+int is_prime_number(int n)
+{
+  if (n < 1)
+    {
+      return (-1);
+    }
+
+  return (is_prime_number(int n));
+}
