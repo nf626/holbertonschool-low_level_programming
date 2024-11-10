e460d0dde98049bcc061cd5f23b58ff5d577d0f7
Nigel Feng <10083@holbertonstudents.com>
2024-11-10 12:16:01 +0000

    4. To infinity and beyond

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - adds positive numbers.
@@ -14,11 +15,17 @@ int main(int argc, char *argv[])
 {
 int sum, i, j;
 
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+
 for (i = 1; i < argc; i++)
 {
-for (j = 0; argv[i][j]; j++)
+for (j = 0; argv[i][j] != '\0'; j++)
 {
-if (argv[i][j] < '0' || argv[i][j] > '9')
+if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
 return (1);
