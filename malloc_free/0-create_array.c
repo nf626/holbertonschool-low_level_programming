132bc20da22f1b22466ee9ac1f083ffb86267b51
Nigel Feng <10083@holbertonstudents.com>
2024-11-11 12:50:30 +0000

    0. Float like a butterfly, sting like a bee

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *create_array(unsigned int size, char c)
 unsigned int i;
 char *ptr;
 
-ptr = malloc(sizeof(*ptr) * size);
-
 if (size == 0)
 {
 return (NULL);
 }
+
+ptr = malloc(sizeof(*ptr) * size);
 
 if (ptr == NULL)
 {
