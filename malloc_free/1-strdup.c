92a20b55d8849c6d595abafa32433c4c0cb70ece
Nigel Feng <10083@holbertonstudents.com>
2024-11-11 13:33:46 +0000

    0. Float like a butterfly, sting like a bee

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *           which contains a copy of the string given as a parameter.
+ * @str: string to copy (Duplicated).
+ *
+ * Return: str = NULL / pointer to the duplicated string.
+ */
+char *_strdup(char *str)
+{
+  int i = 0;
+  char *array;
+
+  if (*str == '\0')
+    {
+      return (NULL);
+    }
+
+  array = malloc(sizeof(char) * 1);
+
+  if (array == NULL)
+    {
+      
+    }
+  return (str);		 
+}
