ee85352044d002a25e1ab55ee7c00f2b5157fb99
Nigel Feng <10083@holbertonstudents.com>
2024-11-12 03:04:51 +0000

    1. The woman who has no imagination has no wings

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,31 +11,24 @@
  */
 char *_strdup(char *str)
 {
-int i = 0, j = 0, length;
+int i = 0;
 char *array;
+
+array = malloc(sizeof(char) * 100);
 
 if (*str == '\0')
 {
 return (NULL);
 }
-while (str[i] != '\0')
-{
-length = length + 1;
-i++;
-}
-array = malloc(sizeof(char) * (length + 1);
+
 while (str[i] != '\0')
 {
 array[i] = str[i];
 i++;
 }
-if (i < length)
-{
-array[i] = '\0';
-i++;
-}
 if (array == NULL)
 {
+free(array);
 return (NULL);
 }
 return (array);
