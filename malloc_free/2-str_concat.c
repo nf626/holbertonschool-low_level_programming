c8235b118071c6f6161c9c8e4ae49f983292486a
Nigel Feng <10083@holbertonstudents.com>
2024-11-12 07:35:58 +0000

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,28 +13,17 @@ char *str_concat(char *s1, char *s2)
 int i = 0, j = 0, k = 0, total = 0;
 char *str;
 if (s1 == NULL)
-{
 s1 = "";
-}
 if (s2 == NULL)
-{
 s2 = "";
-}
 while (s1[i] != '\0')
-{
 i++;
-}
 while (s2[j] != '\0')
-{
 j++;
-}
+
 total = i + j;
 str = malloc(sizeof(char) * (total + 1));
-if (str == NULL)
-{
-free(str);
-return (NULL);
-}
+j = 0;
 while (k < total)
 {
 if (k <= i)
@@ -47,6 +36,11 @@ str[k] = s2[j];
 j++;
 }
 k++;
+}
+if (str == NULL)
+{
+free(str);
+return (NULL);
 }
 str[k] = '\0';
 return (str);
