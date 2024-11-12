377ad70fe90cd5a8073e0e4a08bd7797e697f7d3
Nigel Feng <10083@holbertonstudents.com>
2024-11-12 13:08:01 +0000

    3. If you even dream of beating me you'd better wake up and apologize

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers.
@@ -11,28 +10,44 @@
  */
 int **alloc_grid(int width, int height)
 {
-  int i, j;
-  int **ptr;
-
-  *ptr = malloc(sizeof(int *) * (width * height));
-  /** Failure */
-  if (ptr == NULL)
-    {
-      free(*ptr);
-      return (NULL);
-    }
-  /** if width or height <= 0 */
-  if (width <= 0 || height <= 0)
-    {
-      return (NULL);
-    }
-  /** Row */
-  for (i = 0; i < width; i++)
-    {
-      for (j = 0; j < height; j++)
-	{
-	}
-    }
-  /** Coloumn */
-  return ();
+int i, j, k, l;
+int **array;
+/** if width or height <= 0 */
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+/** Row memory allocation */
+array = malloc(sizeof(int *) * width);
+/** Failure */
+if (array == NULL)
+{
+free(array);
+return (NULL);
+}
+/** Coloumn memory allocation */
+for (i = 0; i < width; i++)
+{
+array[i] = malloc(sizeof(int) * height);
+if (array[i] == NULL)
+{
+for (j = i; j >= 0; j--)
+{
+free(array[j]);
+}
+free(array);
+return (NULL);
+}
+}
+k = 0, l = 0;
+while (k < width)
+{
+while (l < height)
+{
+array[k][l] = 0;
+l++;
+}
+k++;
+}
+return (array);
 }
