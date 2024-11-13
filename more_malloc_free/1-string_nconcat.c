9939d6cbe402187b6e69cf6ddb3a9a1ab9adee4f
Nigel Feng <10083@holbertonstudents.com>
2024-11-13 08:11:31 +0000

    1. string_nconcat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: String 1 - "Best ".
+ * @s2: String 2 - "School !!!".
+ * @n: Size - 6.
+ *
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+  /** Null = empty string */
+  if (s1 == NULL)
+    {
+      s1 = " ";
+    }
+  if (s2 == NULL)
+    {
+      s2 = " ";
+    }
+  /** n >= length s2, use entire string s2 */
+  /** memory failure */
+}
