dbb6ea65e5ef5bdaabd20cf75007fe4e54e4644e
Nigel Feng <10083@holbertonstudents.com>
2024-11-14 00:05:05 +0000

    1. string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,24 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * string_nconcat - concatenates two strings.
- * @s1: String 1 - "Best ".
- * @s2: String 2 - "School !!!".
+ * @s1: String 1 - "Best ". (5)
+ * @s2: String 2 - "School !!!". (10)
  * @n: Size - 6.
  *
+ * Return: newly allocated space in memory, which contains s1
+ *         followed by the first n bytes of s2.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  /** Null = empty string */
-  if (s1 == NULL)
-    {
-      s1 = " ";
-    }
-  if (s2 == NULL)
-    {
-      s2 = " ";
-    }
-  /** n >= length s2, use entire string s2 */
-  /** memory failure */
+unsigned int i = 0, j = 0, k = 0, total = 0;
+char *str;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[i] != '\0')
+i++;
+while (s2[j] != '\0')
+j++;
+if (n >= j)
+total = total + i + j;
+else
+total = total + i + n;
+str = malloc(sizeof(char) * (total + 1));
+if (str == NULL)
+return (NULL);
+j = 0;
+while (k < total)
+{
+if (k <= i)
+{
+str[k] = s1[k];
+}
+if (k >= i)
+{
+str[k] = s2[j];
+j++;
+}
+k++;
+}
+str[k] = '\0';
+return (str);
 }
