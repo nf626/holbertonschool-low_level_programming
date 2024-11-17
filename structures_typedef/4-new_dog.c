21161c504b622dd25113b098f29c9fa3cd728206
Nigel Feng <10083@holbertonstudents.com>
2024-11-17 12:52:03 +0000

    4. A door is what a dog is perpetually on the wrong side of

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -12,12 +11,43 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *dog;
-
-  dog = malloc(sizeof());
-  if (dog == NULL)
-    {
-      free(dog);
-      return (NULL);
-    }
+unsigned int i = 0, j = 0, k = 0;
+dog_t *ndog;
+if (name == NULL || owner == NULL)
+return (NULL);
+while (name[i] != '\0')
+i++;
+while (owner[j] != '\0')
+j++;
+ndog = malloc(sizeof(dog_t));
+if (ndog == NULL)
+{
+free(ndog);
+return (NULL);
+}
+ndog->name = malloc(sizeof(char) * i);
+if (ndog->name == NULL)
+{
+free(ndog);
+return (NULL);
+}
+while (k < i)
+{
+ndog->name[k] = name[k];
+k++;
+}
+ndog->age = age;
+ndog->owner = malloc(sizeof(char) * j);
+if (ndog->owner == NULL)
+{
+free(ndog);
+return (NULL);
+}
+k = 0;
+while (k < j)
+{
+ndog->owner[k] = owner[k];
+k++;
+}
+return (ndog);
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,10 +18,12 @@ char *owner;
 
 /**
  * New name for structure dog.
+ * typedef = alias.
  */ 
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
