c14cb90f559287c8ce031fcedc677bd0450bfbac
Nigel Feng <10083@holbertonstudents.com>
2024-11-17 06:57:16 +0000

    0. Poppy

---
+++ structures_typedef/README.md
@@ -0,0 +1,5 @@
+C - Structures, typedef Tasks:<br>
+0. Poppy - Define a new type struct dog with the following elements:
+name, type = char *.<br>
+age, type = float. <br>
+owner, type = char *.<br>
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE_NAME
+#define HEADER_FILE_NAME
+
+/**
+ * struct dog - structure.
+ * @name: type = char *.
+ * @age: type = float.
+ * @owner: type = char *.
+ *
+ * Description: Define a new type struct dog with the following elements:
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
