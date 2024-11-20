2015d8ce68cbeee7756df68ac68d4049553393b2
NIgel Feng <10083@holbertonstudents.com>
2024-11-20 00:12:39 +0000

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - selects the correct function
@@ -11,23 +12,19 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
-    
-    while (i < 5)
-      {
-	if ()
-	  {
-	   ops[i].
-	  }
-	i++;
-      }
-    return (s);
+op_t ops[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul},
+	      {"/", op_div}, {"%", op_mod}, {NULL, NULL}};
+int i;
+
+while (i < 5)
+{
+/** s does not match any of the 5 expected operators */
+if (strcmp(s, ops[i].op) == 0)
+{
+/** allow to access struct op - member *f */
+return (ops[i].f);
+}
+i++;
+}
+return (0);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,26 +1,29 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - calculate the sum.
+ * @argc: number of arguments.
+ * @argv: number of argument vector (strings).
  *
  * Return: sum of calc.
  */
 int main(int argc, char *argv[])
 {
-  int (*operator)(int num1, int num2);
-  
-  if (argc != 4)
-    {
-      printf("Error\n");
-      exit(98);
-    }
-  operator;
-  if (operator == 0)
-    {
-      printf("Error\n");
-      exit(99)
-    }
-  printf("%d\n", atoi(argv[]), atoi(argv[]));
-  return (0);
+int (*operator)(int num1, int num2);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+operator = get_op_func(argv[2]);
+if (operator == 0)
+{
+printf("Error\n");
+exit(99);
+}
+printf("%d\n", operator(atoi(argv[1]), atoi(argv[3])));
+return (0);
 }
