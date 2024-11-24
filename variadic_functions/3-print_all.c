0d72eec381f47c06af9a8a6e327995a27ab85211
Nigel Feng <10083@holbertonstudents.com>
2024-11-24 06:23:06 +0000

    3. To be is a to be the value of a variable

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,16 +2,53 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-typedef struct {
-  char *ptr;
-  void (*function_ptr)(char *comma, va_list args); /** separator ',' */
-} format_list;
+/**
+ * print_all -  function that prints anything.
+ * @format: list of types of arguments passed to the function.
+ *
+ */
+void print_char(char *comma, va_list args)
+{
+  printf("%s%c\n", comma, va_arg(args, int));
+}
 
-/** function declaration/prototype */
-void print_char(char *comma, );
-void print_int(char *comma, );
-void print_float(char *comma, );
-void print_char_pointer(char *comma, );
+/**
+ * print_int -  function that prints integers.
+ * @comma: list of types of arguments passed to the function.
+ * @args:
+ *
+ */
+void print_int(char *comma, va_list args)
+{
+  printf("%s%d\n", comma, va_arg(args, int));
+}
+
+/**
+ * print_all -  function that prints anything.
+ * @format: list of types of arguments passed to the function.
+ *
+ */
+void print_float(char *comma, va_list args)
+{
+  printf("%s%f\n", comma, va_arg(args, double));
+}
+
+/**
+ * print_char_pointer -  function that prints anything.
+ * @format: list of types of arguments passed to the function.
+ *
+ */
+void print_char_pointer(char *comma, va_list args)
+{
+  char *str;
+  str = va_arg(args, char *); 
+  if (str == NULL)
+    {
+      printf("%s%s", comma, "(nil)");
+      return;
+    }
+  printf("%s%s\n", comma, str);
+}
 
 /**
  * print_all -  function that prints anything.
@@ -22,49 +59,29 @@ void print_char_pointer(char *comma, );
 /** print_all("ceis", 'B', 3, "stSchool"); */
 void print_all(const char * const format, ...)
 {
+  unsigned int i = 0, j = 0;
+  char *comma = "";
+  va_list args;
   /** structure stores format_types variable into 2D array = *prt -> c, *function_ptr -> print_char 
       eg. array[0] = c, go into print_char function */
-  format_list format_types = {{"c", print_char},
-			      {"i", print_int},
-			      {"f", print_float},
-			      {"s", print_char_pointer}};
-  va_list args;
+  format_list format_types[] = {{"c", print_char}, {"i", print_int},
+			      {"f", print_float}, {"s", print_char_pointer}};
+
   va_start(args, format); /** points to first element in variadic function eg. B -> 3 -> stSchool */
 
-  va_end(args);
-
-  return;
-}
-
-/** function definition - void return nothing */
-void print_char(char *comma, va_list args)
-{
-  printf("%s%c\n", comma, );
-}
-
-void print_int(char *comma, va_list args)
-{
-  printf("%s%d\n", comma, );
-}
-
-void print_float(char *comma, va_list args)
-{
-  printf("%s%f\n", comma, );
-}
-
-void print_char_pointer(char *comma, va_list args)
-{
-  char *str;
-  /** assign the string (the pointer to the first character of the string) to str. 
-      extracts the next argument from the args list, expecting it to be a string (char *),
-      and assigns it to the variable str.
-      retrieves the next argument from the args list, and it's expected to be a char * (a string).
-      The returned value is stored in the pointer str.
-  */
-  str = va_arg(args, char *); 
-  if (str == NULL)
+  while (format != NULL && format[i] != 0) /** format not empty and is true */
     {
-      printf("%s%s", comma, "(nil)");
+      while (j < 4)
+	{
+	  if (format[i] == *format_types[j].ptr)
+	    {
+	      format_types[j].function_ptr(comma, args);
+	      comma = ",";
+	    }
+	  j++;
+	}
+      i++;
     }
-  printf("%s%s\n", comma, str);
+  va_end(args);
+  printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,14 @@
+#include <stdarg.h>
 #ifndef VARIADIC_HEAD
 #define VARIADIC_HEAD
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+typedef struct {
+  char *ptr;
+  void (*function_ptr)(char *comma, va_list args);
+} format_list;
 
 #endif
