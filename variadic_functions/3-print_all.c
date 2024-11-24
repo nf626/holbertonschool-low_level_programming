69f3b751e9a40467c05fd3cb56573ca5cc6002b6
Nigel Feng <10083@holbertonstudents.com>
2024-11-24 03:27:47 +0000

    3. To be is a to be the value of a variable

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+typedef struct {
+  char *ptr;
+  void (*function_ptr)();
+} format_list;
+
+/** function declaration/prototype */
 void print_char();
 void print_int();
 void print_float();
@@ -12,7 +18,41 @@ void print_char_pointer();
  * @format: list of types of arguments passed to the function.
  *
  */
+
+/** print_all("ceis", 'B', 3, "stSchool"); */
 void print_all(const char * const format, ...)
 {
+  /** structure 2D array - *prt -> c, *function_ptr ->  */
+  format_list format_types = {{"c", },
+			      {"i", },
+			      {"f", },
+			      {"s", }};
+  va_list args;
+  va_start(args, format);
 
+  va_end(args);
+
+  return;
+}
+
+/** function definition - void return nothing */
+void print_char()
+{
+  printf("%c\n", );
+}
+void print_int()
+{
+  printf("%d\n",);
+}
+void print_float()
+{
+  printf("%f\n",);
+}
+void print_char_pointer()
+{
+  if (string == NULL)
+    {
+      printf("%s\n", "(nil)");
+    }
+  printf("%s\n",);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,9 +5,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-typedef struct data_types {
-  char *type;
-  void (*func)();
-} list;
-
 #endif
