1e921919b2cdf5852ee7acfd9699f734bd8b9dec
Nigel Feng <10083@holbertonstudents.com>
2024-11-30 06:40:01 +0000

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -16,37 +16,37 @@ int main(void)
 int num1 = '0', num2, num3;
 
 while (num1 <= '9')
-  {
-    num2 = '0';
-    while (num2 <= '9')
-      {
-	num3 = '0';
-	while (num3 <= '9')
-	  {
-	    if ((num1 == num2) || (num1 == num3) || (num2 == num3))
-	      {
-	      }
-	    else if ((num1 > num2) || (num1 > num3) || (num2 > num3))
-	      {
-	      }
-	    else
-	      {
-		putchar(num1);
-		putchar(num2);
-		putchar(num3);
-		if ((num1 == '7') && (num2 == '8') && (num3 == '9'))
-		  {
-		    break;
-		  }
-		putchar(',');
-		putchar(' ');
-	      }
-	    num3++;
-	  }
-	num2++;
-      }
-    num1++;
-  }
+{
+num2 = '0';
+while (num2 <= '9')
+{
+num3 = '0';
+while (num3 <= '9')
+{
+if ((num1 == num2) || (num1 == num3) || (num2 == num3))
+{
+}
+else if ((num1 > num2) || (num1 > num3) || (num2 > num3))
+{
+}
+else
+{
+putchar(num1);
+putchar(num2);
+putchar(num3);
+if ((num1 == '7') && (num2 == '8') && (num3 == '9'))
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+num3++;
+}
+num2++;
+}
+num1++;
+}
 putchar('\n');
 return (0);
 }
