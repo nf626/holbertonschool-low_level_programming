1e58c78f5ac65c69db29ec8a63eb973c50be2d30
Nigel Feng <10083@holbertonstudents.com>
2024-12-01 03:33:49 +0000

    13. Nature made the natural numbers; All else is the work of women

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -8,16 +8,16 @@
  */
 int main(void)
 {
-  int n, sum = 0;
+int n = 0, sum = 0;
 
-  if (n < 1024)
-    {
-      while (n % 3 || n % 5)
-	{
-	  sum = sum + n;
-	  n++;
-	}
-      
-    }
-  return (0);
+while (n < 1024)
+{
+if ((n % 3) == 0 || (n % 5) == 0)
+{
+sum = sum + n;
+}
+n++;
+}
+printf("%d\n", sum);
+return (0);
 }
