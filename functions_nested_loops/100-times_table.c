256fbb9b3151c0d23314026f81a32e3317c3ff84
Nigel Feng <10083@holbertonstudents.com>
2024-12-01 02:09:57 +0000

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -3,53 +3,47 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0.
- * If n is greater than 15 or less than 0 the function should not print anything.
+ * If n is greater than 15 or less than 0
+ * the function should not print anything.
  * @n: number of times table.
- *
- * Return: Always 0.
  */
 void print_times_table(int n)
 {
-  int x = 0, y, sum = 0, one, ten, hund;
-
-  if (n >= 0 && n <= 15)
-    {
-      while (x <= n)
-	{
-	  y = 0;
-	  while (y <= n)
-	    {
-	      sum = x * y;
-	      one = sum % 10;
-	      ten = (sum / 10) % 100;
-	      hund = sum / 100;
-	      if (hund == 0 && y != 0)
-		{
-		  _putchar(' ');
-		  if (ten == 0)
-		    {
-		      _putchar(' ');
-		    }
-		  else
-		    {
-		      _putchar(ten + '0');
-		    }
-		}
-	      else if (hund != 0)
-		{
-		  _putchar(hund + '0');
-		  _putchar(ten + '0');
-		}
-	      _putchar(one + '0');
-	      if (y != n)
-		{
-		  _putchar(',');
-		  _putchar(' ');
-		}
-	      y++;
-	    }
-	  _putchar('\n');
-	  x++;
-	}
-    }
+int x = 0, y, sum = 0, one, ten, hund;
+if (n >= 0 && n <= 15)
+{
+while (x <= n)
+{
+y = 0;
+while (y <= n)
+{
+sum = x * y;
+one = sum % 10;
+ten = (sum / 10) % 100;
+hund = sum / 100;
+if (hund == 0 && y != 0)
+{
+_putchar(' ');
+if (ten == 0)
+_putchar(' ');
+else
+_putchar(ten + '0');
+}
+else if (hund != 0)
+{
+_putchar(hund + '0');
+_putchar(ten + '0');
+}
+_putchar(one + '0');
+if (y != n)
+{
+_putchar(',');
+_putchar(' ');
+}
+y++;
+}
+_putchar('\n');
+x++;
+}
+}
 }
