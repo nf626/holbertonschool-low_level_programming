736ab913b9190d40afa18fad24012d7110c85dde
Nigel Feng <10083@holbertonstudents.com>
2024-12-01 06:18:44 +0000

    15. Even Liber Abbaci

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued terms,
+ *        followed by a new line.
+ *
+ * Return: 0, Success.
+ */
+int main(void)
+{
+  long prev = 0, next = 1, sum = 0, total = 0;
+
+ while (sum < 4000000)
+   {
+     sum = prev + next;
+     if (sum % 2 == 0)
+       {
+	 total = total + sum;
+       }
+     prev = next;
+     next = sum;
+   }
+ printf("%ld\n", total);
+return (0);
+}
