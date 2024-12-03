8533dd0fa1e8f51f7d63532ee68c52b6a5b245a6
Nigel Feng <10083@holbertonstudents.com>
2024-12-03 10:48:36 +0000

    4. Free list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -11,14 +11,14 @@
  */
 void free_list(list_t *head)
 {
-  list_t *temp = NULL;
+list_t *temp = NULL;
 
-  while (head != NULL)
-    {
-      temp = head;
-      head = head->next;
-      free(temp->str);
-      free(temp);
-    }
-  free(head);
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp->str);
+free(temp);
+}
+free(head);
 }
