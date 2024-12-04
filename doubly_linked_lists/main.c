f30fe8c6dcf9ccbef56ddebd4cddcd7e367c7173
Nigel Feng <10083@holbertonstudents.com>
2024-12-04 03:18:47 +0000

    2. Add node

--- doubly_linked_lists/main.c
+++ doubly_linked_lists/main.c
@@ -14,9 +14,9 @@ int main(void)
 
     head = NULL;
     add_dnodeint(&head, 0);
-    add_dnodeint(&head, 1);
-    add_dnodeint(&head, 2);
-    add_dnodeint(&head, 3);
+    add_dnodeint(&head, 0);
+    add_dnodeint(&head, 0);
+    add_dnodeint(&head, 0);
     add_dnodeint(&head, 4);
     add_dnodeint(&head, 98);
     add_dnodeint(&head, 402);
