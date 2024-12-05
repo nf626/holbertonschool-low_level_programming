bd9abf805ef6908bbc69f0a1030e7b68319517e3
Nigel Feng <10083@holbertonstudents.com>
2024-12-05 13:30:17 +0000

    7. Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,45 +17,46 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 unsigned int count;
 dlistint_t *new, *current, *prev;
-current = *h;
-if (*h == NULL && idx == 0)
-return (NULL);
-new = malloc(sizeof(dlistint_t));
-if (new == NULL)
-{
-free(new);
-return (NULL);
-}
-if (*h != NULL)
-{
-count = 0;
-prev = NULL;
-while (current->prev != NULL)
-current = current->prev;
-while (current != NULL && count < idx)
-{
-prev = current;
-current = current->next;
-count++;
-}
-if (count == idx)
-{
-new->n = n;
-new->prev = prev;
-if (current != NULL)
-current->prev = new;
-new->next = current;
-if (idx != 0)
-prev->next = new;
-else
-*h = new;
-return (new);
-}
-return (NULL);
-}
-new->next = NULL;
-new->prev = NULL;
-new->n = n;
-*h = new;
-return (new);
+ current = *h;
+ if (*h == NULL)
+   {
+     current->next = NULL;
+     *h = current;
+     return (*h);
+   }
+ if (*h == NULL && idx == 0)
+   return (NULL);
+ new = malloc(sizeof(dlistint_t));
+ if (*h != NULL)
+   {
+     count = 0;
+     prev = NULL;
+     while (current->prev != NULL)
+       current = current->prev;
+     while (current != NULL && count < idx)
+       {
+	 prev = current;
+	 current = current->next;
+	 count++;
+       }
+     if (count == idx)
+       {
+	 new->n = n;
+	 new->prev = prev;
+	 if (current != NULL)
+	   current->prev = new;
+	 new->next = current;
+	 if (idx != 0)
+	   prev->next = new;
+	 else
+	   *h = new;
+	 return (new);
+       }
+     return (NULL);
+   }
+ new->next = NULL;
+ new->prev = NULL;
+ new->n = n;
+ *h = new;
+ return (new);
 }
