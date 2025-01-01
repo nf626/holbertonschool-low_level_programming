315089817ea8127654638fe5d72d4d174943b1c0
Nigel Feng <10083@holbertonstudents.com>
2025-01-01 08:40:01 +0000

    1. Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,20 +1,35 @@
 #include "main.h"
 
+/**
+ * create_file - Function that creates a file.
+ * @filename: name of the file to create.
+ * @text_content: string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
 int create_file(const char *filename, char *text_content)
 {
-  int fd;
+  int fd, i = 0;;
 
   if (filename == NULL)
     {
       return (-1);
-    } 
-  if (text_content == NULL)
-    {
-      fd = open(filename, O_RDWR | O_CREAT); 
     }
-  else
+  
+  fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+      {
+	return (-1);
+      }
+
+  while (text_content[i] != '\0')
     {
-      fd = open(filename, O_RDWR | O_CREAT | O_EXCL | O_TRUNC);
+      i = i + 1;
+    }
+
+   if (text_content != NULL)
+    {
+      write (fd, text_content, i);
     }
 
   close(fd);
