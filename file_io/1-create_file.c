7ff0cb147f2cc1cf4f74b6a487f91ec611143a3a
Nigel Feng <10083@holbertonstudents.com>
2025-01-01 08:42:44 +0000

    1. Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,29 +9,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-  int fd, i = 0;;
+int fd, i = 0;
 
-  if (filename == NULL)
-    {
-      return (-1);
-    }
-  
-  fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-      {
-	return (-1);
-      }
+if (filename == NULL)
+{
+return (-1);
+}
 
-  while (text_content[i] != '\0')
-    {
-      i = i + 1;
-    }
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+{
+return (-1);
+}
 
-   if (text_content != NULL)
-    {
-      write (fd, text_content, i);
-    }
+while (text_content[i] != '\0')
+{
+i = i + 1;
+}
 
-  close(fd);
-  return (1);
+if (text_content != NULL)
+{
+write(fd, text_content, i);
+}
+
+close(fd);
+return (1);
 }
