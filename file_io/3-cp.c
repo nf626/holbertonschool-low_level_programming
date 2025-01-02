804c621e5dbc77faac62d0e37f5fa1b7e3665d0e
root <10083@holbertonstudents.com>
2025-01-02 06:36:19 +0000

    3. cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,9 +18,8 @@ void copy_file(const char *source, const char *dest)
     }
 
   dest_fd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-  rd = read(source_fd, buffer, 1024);
 
-  while (rd > 0)
+  while ((rd = read(source_fd, buffer, 1024) > 0)
     {
       wr = write(dest_fd, buffer, rd);
       if (wr != rd || dest_fd == -1)
